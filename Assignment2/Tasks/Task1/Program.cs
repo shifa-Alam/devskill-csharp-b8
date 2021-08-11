775f688e4e04583ace8b0459fa22273490c84092
Shifa Alam <shifa7446@gmail.com>
2021-08-11 11:26:48 +0600

    task one done.

---
+++ Assignment2/Tasks/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = IsPrime(19);
+            Console.WriteLine(result);
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number == 2)
+            {
+                return true;
+            }
+            else if (number > 2)
+            {
+                for (var i = 2; i <= number / 2; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+
+                    }
+                    else return true;
+                }
+            }
+           
+            return false;
+
+        }
+    }
+}
