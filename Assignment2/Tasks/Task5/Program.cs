f484654b051d5b529bde027da499b93f899c8fdc
Shifa Alam <shifa7446@gmail.com>
2021-08-11 12:12:26 +0600

    task 5 done

--- Assignment2/Tasks/Task5/Program.cs
+++ Assignment2/Tasks/Task5/Program.cs
@@ -7,13 +7,34 @@ namespace Task5
     {
         static void Main(string[] args)
         {
-            var result = CountVowels("Hellow World");
+            var result = CountVowels("Hellow WOrld");
             Console.WriteLine(result);
         }
 
         public static int CountVowels(string aText)
         {
-            throw new NotImplementedException();
+            var count = 0;
+            for (var i = 0; i < aText.Length; i++)
+            {
+                if (
+                    (aText[i].ToString().Equals("a", StringComparison.InvariantCultureIgnoreCase)) ||
+                    (aText[i].ToString().Equals("e", StringComparison.InvariantCultureIgnoreCase)) ||
+                    (aText[i].ToString().Equals("i", StringComparison.InvariantCultureIgnoreCase)) ||
+                    (aText[i].ToString().Equals("o", StringComparison.InvariantCultureIgnoreCase)) ||
+                    (aText[i].ToString().Equals("u", StringComparison.InvariantCultureIgnoreCase))
+
+                    )
+                {
+                    count++;
+                }
+
+            }
+            //if (aText.Contains("a",StringComparison.OrdinalIgnoreCase)) { count++; }
+            //if (aText.Contains("e", StringComparison.OrdinalIgnoreCase)) { count++; }
+            //if (aText.Contains("i", StringComparison.OrdinalIgnoreCase)) { count++; }
+            //if (aText.Contains("o", StringComparison.OrdinalIgnoreCase)) { count++; }
+            //if (aText.Contains("u", StringComparison.OrdinalIgnoreCase)) { count++; }
+            return count;
         }
     }
 }
