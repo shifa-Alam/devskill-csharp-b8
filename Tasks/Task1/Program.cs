b73bac1484b6e5a858199632efabb364b7b617c8
Shifa Alam <shifa7446@gmail.com>
2021-08-19 22:15:28 +0600

    f

---
+++ Tasks/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = ConvertTime("1:01 pm");
+            Console.WriteLine(result);
+        }
+
+        public static string ConvertTime(string twelveHourFormat)
+        {
+            if (twelveHourFormat.Contains("am"))
+            {
+                twelveHourFormat = twelveHourFormat.Replace("am", "");
+
+                var m = twelveHourFormat.Split(":");
+                var x = Convert.ToInt32(m[0]);
+                var n = 0;
+                if (x == 12)
+                {
+                    n = x - 12;
+                }
+                twelveHourFormat = twelveHourFormat.Replace(x.ToString(), n.ToString());
+                
+            }
+            else if(twelveHourFormat.Contains("pm"))
+            {
+                twelveHourFormat= twelveHourFormat.Replace("pm", "");
+                var m = twelveHourFormat.Split(":");
+                var x = Convert.ToInt32(m[0]);
+                var n = 0;
+                if (x < 12)
+                {
+                    n = x + 12;
+                }
+                twelveHourFormat = twelveHourFormat.Replace(x.ToString(), n.ToString());
+            }
+            else
+            {
+                twelveHourFormat = twelveHourFormat.Replace("pm", "");
+                twelveHourFormat = twelveHourFormat.Replace("am", "");
+            }
+            
+            return twelveHourFormat.Trim();
+        }
+    }
+}
---
+++ Tasks/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Rotate("aaabbcc", 2);
+            Console.WriteLine(result);
+        }
+
+        public static string Rotate(string original, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
