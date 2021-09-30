36e656149a7617bc7d9b3bc2b13b053e92453d4a
shifa-Alam <shifa7446@gmail.com>
2021-09-30 23:19:56 +0600

    updated

--- Tasks/Task5/Program.cs
+++ Tasks/Task5/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 namespace Task5
 {
     public class Program
@@ -7,6 +7,12 @@ namespace Task5
         static void Main(string[] args)
         {
             // Write your code here. No unit test for this task.
+
+            DirectoryInfo aDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
+            var parent = aDirectory.Parent.Parent.Parent;
+            //using var fs = File.Create(Path.Combine(parent.FullName, "hello.txt"));
+            string fullPath = Path.Combine(parent.FullName, "hello.txt");
+            File.WriteAllText(fullPath, "Hello world from C#");
         }
     }
 }
